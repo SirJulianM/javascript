dfc946f6ae119c801638019e5986531dea07b411
Freddie8606 <ing_julianmadrid@hotmail.com>
2020-11-26 23:47:19 -0500

    Hola mundo

---
+++ 01-holamundo.js
@@ -0,0 +1,74 @@
+/*
+Notas: 
+
+Javascript es el único lenguaje capaz de ejecutarse en las tres capas de una aplicación
+
+Front(Javascript)
+Back(Node.js)
+Persistencia de datos(MongoDB, Firebase)
+
+Características
+
+- Lenguaje de alto nivel
+- Interpretado
+- Dinámico
+- Debilmente tipado
+- Multiparadigma
+- Sensible a mayúsculas y minúsculas
+- No necesitas punto y comas al final de cada línea
+
+Escritura de código
+
+* Los identificadores deben comenzar con algunos de estos elementos
+
+    - Una letra
+    - Un signo de dólar
+    - Un guión bajo
+    - Nunca con números o caracteres especiales
+
+* Usa snake_case en
+
+    - Archivos
+
+* Usa UPPER_CASE en
+
+    - Constantes
+
+* Usa UpperCamelCase en
+
+    - Clases
+
+* Usa lowerCamelCase en 
+
+    - Objetos
+    - Primitivos 
+    - Funciones
+    - Instancias
+
+Ordenamiento de código
+
+- Importación de módulos
+- Declaración de variables
+- Declaración de funciones
+- Ejecución de código
+
+Tipos de datos en Javascript
+
+1) Primitivos: Se acceden directamente al valor
+    - string
+    - number
+    - boolean
+    - null
+    - undefined
+    - NaN
+
+2) Compuestos: Se accede a la referencia del valor
+    - Objetos {}
+    - Arrays []
+    - Funciones () {}
+    - Clases {}
+    - etc
+
+*/
+
+console.log('Hola mundo')
