30aacf21727cb9add8c1d406211c984881a9bc31
Freddie8606 <ing_julianmadrid@hotmail.com>
2020-11-27 11:49:34 -0500

    ACP

---
+++ 29-ACP.js
@@ -0,0 +1,14 @@
+/* Alert, Confirm y Promp */
+
+console.log(window);
+window.alert('Esto es una alerta');
+window.confirm('Esto es una confirmación');
+prompt('Hola esto es un aviso y te permite ingresar un valor');
+
+let alerta = alert('Hola, esto es una alerta'),
+confirmacion = confirm('Hola, esto es una confirmación'),
+aviso = prompt('Hola, esto es un aviso y le permite el ingreso de un valor');
+
+console.log(alerta);
+console.log(confirmacion);
+console.log(aviso);
