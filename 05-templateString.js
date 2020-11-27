4cd0f8cf41542c803ec6cda7d3e9d4df5925d39c
Freddie8606 <ing_julianmadrid@hotmail.com>
2020-11-26 23:54:30 -0500

    template String

---
+++ 05-templateString.js
@@ -0,0 +1,26 @@
+let nombre = "Mónica";
+let apellido = "Hincapié"
+
+console.log("Hola, mi nombre es", nombre, apellido)
+
+//Concatenación
+
+let bienvenido = "Bienvenido(a): "+ nombre + " " + apellido
+console.log(bienvenido);
+
+//Interpolación de variables
+//Template String
+
+let saludo = `Hola, mi nombre es ${nombre} ${apellido}`;
+
+console.log(saludo);
+
+let ul = `
+        <ul>
+            <li>Primavera</li>
+            <li>Verano</li>
+            <li>Otoño</li>
+            <li>Invierno</li>
+        </ul>`;
+
+console.log(ul);
