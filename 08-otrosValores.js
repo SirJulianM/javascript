938f163a1904e7160c5f545907178e80e664071c
Freddie8606 <ing_julianmadrid@hotmail.com>
2020-11-27 00:01:17 -0500

    NaN undefined

---
+++ 08-otrosValores.js
@@ -0,0 +1,14 @@
+/* Undefined: Indica que no se ha inicializado una variable y que el valor está ausente */
+
+let definida;
+//console.log(indefinida);
+
+/* Null: Es un valor especial que indica la ausencia de un valor */
+
+let nulo = null;
+console.log(nulo);
+
+/* NaN - Not a Number (no es un número) */
+
+let noEsUnNumero = "hola"*3.7;
+console.log(noEsUnNumero);
