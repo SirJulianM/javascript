240e02d2c0c68994a94ebb53db5a47d7520b7ee6
Freddie8606 <ing_julianmadrid@hotmail.com>
2020-11-27 11:48:05 -0500

    Potenciación, radicación y otros

---
+++ 27-objetoMath.js
@@ -0,0 +1,10 @@
+console.log(Math.PI); //Devuelve el número pi
+console.log(Math.abs(-7.8)); //Devuelve el número en valor absoluto
+console.log(Math.ceil(1.9)); //Aproxima al numero más cercano
+console.log(Math.floor(9.5)); //Quita el número decimal
+console.log(Math.round(10.6)); //Redondea el número 
+console.log(Math.sqrt(81)); //Raiz cuadrada
+console.log(Math.pow(2,5)); //Potenciación 
+console.log(Math.sign(8)); //Te devuelve -1 si es negativo, uno si es positivo y 0 si es neutro
+console.log(Math.random()*6); //Escoge un número aleatorio entre uno y seis
+
