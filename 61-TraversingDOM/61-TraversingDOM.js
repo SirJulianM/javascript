07615bc720285cd8fb8eb1cfe466e1bc7293f034
Freddie8606 <ing_julianmadrid@hotmail.com>
2021-07-03 23:28:52 -0500

    Traversing DOM

---
+++ 61-TraversingDOM/61-TraversingDOM.js
@@ -0,0 +1,15 @@
+const $cards = document.querySelector(".cards");
+console.log($cards);
+console.log($cards.children);
+console.log($cards.children[2]);
+console.log($cards.parentElement);
+console.log($cards.parentNode);
+console.log($cards.firstElementChild);
+console.log($cards.lastElementChild);
+console.log($cards.previousSibling);
+console.log($cards.previousElementSibling);
+console.log($cards.nextSibling);
+console.log($cards.nextElementSibling);
+console.log($cards.closest("div"));
+console.log($cards.closest("body"));
+console.log($cards.children[3].closest("section"));
