0fce610f5656a9eed77e7e01c3fdccd91057e3da
SirJulianM <ing_julianmadrid@hotmail.com>
2021-10-31 10:47:47 -0500

    Tema oscuro

--- 75-EjerciciosDOM2/75-EjerciciosDOM2.js
+++ 75-EjerciciosDOM2/75-EjerciciosDOM2.js
@@ -3,14 +3,16 @@ import {digitalClock, alarm} from "./reloj.js"
 import {moveBall, shortcuts} from "./teclado.js";
 import countdown from './cuentaRegresiva.js';
 import scrollTopButton from "./botonScroll.js";
+import darkTheme from "./temaOscuro.js";
 
 const d = document;
 d.addEventListener("DOMContentLoaded", (e) => {
     hamburgerMenu(".panel-btn", ".panel", ".menu a");
     digitalClock('#reloj', '#activarReloj', '#desactivarReloj');
     alarm('sound/ringtones-pink-panther.mp3', '#activarAlarma', '#desctivarAlarma');
-    countdown("countdown", 'Sep 18, 2021 15:00:00', 'El reencuentro');
+    countdown("countdown", 'Sep 18, 2022 15:00:00', 'El reencuentro');
     scrollTopButton(".scroll-top-btn");
+    darkTheme(".dark-theme-btn", "dark-mode");
 })
 
 d.addEventListener("keydown", (e)=>{
