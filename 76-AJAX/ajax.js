c5cc935864224553ac4ff75417b684077885450d
SirJulianM <ing_julianmadrid@hotmail.com>
2022-01-12 17:10:47 -0500

    fetch AJAX

--- 76-AJAX/ajax.js
+++ 76-AJAX/ajax.js
@@ -38,3 +38,39 @@
     /* 4) Enviar la petición */
     xhr.send();
 })();
+
+(() =>
+{
+    const $fetch = document.getElementById("fetch"),
+    $fragment = document.createDocumentFragment();
+
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res)=> res.ok ? res.json() : Promise.reject(res))
+    .then(
+        json =>
+        {
+            console.log(json);
+            //$fetch.innerHTML = json;
+            json.forEach(el =>
+            {
+                const $li = document.createElement("li");
+                $li.innerHTML = `Nombre: ${el.name} -- Email: ${el.email} -- Teléfono: ${el.phone}`;
+                $fragment.appendChild($li);
+            });
+            $fetch.appendChild($fragment);
+        }
+    )
+    .catch(err =>
+        {
+            console.log(err);     
+            let message = err.statusText || "Ocurrió un error";
+            $fetch.innerHTML = `Error ${err.status}: ${message}`;       
+        }
+    )
+    .finally(() =>
+        {
+            console.log("Esto se ejecutará independientemente del resultado de la promesa fetch");
+        }
+    );
+
+})();
